37d6753a7e75790dadeb94d392fbde125836563e
necro-mancer <cryptosauron@protonmail.com>
2021-10-14 22:18:13 +0200

    Updates

---
+++ _next/static/bER7vik2oMBMcBw-18bB7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-b0c34db3e117517e9f82.js"],"/_error":["static/chunks/pages/_error-e1bcaa043b7e8bc8fc85.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
