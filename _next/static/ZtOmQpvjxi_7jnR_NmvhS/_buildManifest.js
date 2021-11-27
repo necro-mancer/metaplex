1fa81c270f006bd64d4b2a9f3d45a19a06d302da
necro-mancer <cryptosauron@protonmail.com>
2021-11-27 13:42:02 +0100

    Updates

---
+++ _next/static/ZtOmQpvjxi_7jnR_NmvhS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-401a76a3c94891b3498d.js"],"/_error":["static/chunks/pages/_error-e1bcaa043b7e8bc8fc85.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
